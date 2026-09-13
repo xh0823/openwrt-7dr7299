c90cb92fb437f6317346ffae1755e4109941c01a
xh0823 <62717864+xh0823@users.noreply.github.com>
2026-09-13 19:21:44 +0800

    Update diy-6.6.sh

--- scripts/diy-6.6.sh
+++ scripts/diy-6.6.sh
@@ -2,18 +2,18 @@
 set -e -o pipefail
 
 WORKSPACE_ROOT="${GITHUB_WORKSPACE:-$(pwd)}"
-GOLANG126_SRC_DIR="$WORKSPACE_ROOT/scripts/6.6/golang1.26"
-GOLANG126_FEED_DIR="feeds/packages/lang/golang1.26"
-rm -rf "$GOLANG126_FEED_DIR"
-mkdir -p "$GOLANG126_FEED_DIR"
-cp -rf "$GOLANG126_SRC_DIR/." "$GOLANG126_FEED_DIR/"
+GOLANG127_SRC_DIR="$WORKSPACE_ROOT/scripts/6.6/golang1.27"
+GOLANG127_FEED_DIR="feeds/packages/lang/golang1.27"
+rm -rf "$GOLANG127_FEED_DIR"
+mkdir -p "$GOLANG127_FEED_DIR"
+cp -rf "$GOLANG127_SRC_DIR/." "$GOLANG127_FEED_DIR/"
 ./scripts/feeds update -f packages
-./scripts/feeds install golang1.26
+./scripts/feeds install golang1.27
 
-# passwall daed use golang1.26/host
+# passwall daed use golang1.27/host
 find package/dae package/passwall-packages -name "Makefile" -type f -exec sed -i \
-  -e 's|\<golang/golang-package.mk\>|golang1.26/golang-package.mk|g' \
-  -e 's|\<golang/host\>|golang1.26/host|g' {} +
+  -e 's|\<golang/golang-package.mk\>|golang1.27/golang-package.mk|g' \
+  -e 's|\<golang/host\>|golang1.27/host|g' {} +
 
 # 同步仓库内维护的 patches 目录到 OpenWrt 源码树
 if [ -d "$GITHUB_WORKSPACE/patches/6.6" ]; then
